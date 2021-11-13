e4a0170707590d0dc61c79d03c70ad9a0716f782
unknown <jorra04@my.yorku.ca>
2021-11-13 17:51:58 -0500

    Added the game over screen. Need to fix the V, add the M and R.

--- Final Project/EECS3201Final.v
+++ Final Project/EECS3201Final.v
@@ -55,7 +55,7 @@ module EECS3201Final(clkin,rst,MISO,hsync,vsync,r,g,b,MOSI,spiclk,chipselect, he
 	
 	output[6:0] hex01, hex02, hex03, hex04; 
 	
-	ScoreCounter score(clkin, rst, hex01, hex02, hex03, hex04);
+	ScoreCounter score(clkin, rst, gameOver, hex01, hex02, hex03, hex04);
 
 	//horizontal timings
 	parameter HA_END = 639;
@@ -69,12 +69,36 @@ module EECS3201Final(clkin,rst,MISO,hsync,vsync,r,g,b,MOSI,spiclk,chipselect, he
 	parameter VS_END = VS_STA + 2;
 	parameter SCREEN = 524;
 	
+	//writing sizes
+	parameter BLOCK_SIZE = 16;
+	parameter LETTER_SPACING = 70;
+	
+	parameter FIRST_BLOCK_INDENT_START = 84;
+	parameter FIRST_BLOCK_INDENT_END = FIRST_BLOCK_INDENT_START + LETTER_SPACING; //In case we want to change the letter spacing
+	
+	parameter SECOND_BLOCK_INDENT_START = FIRST_BLOCK_INDENT_END + 70;
+	parameter SECOND_BLOCK_INDENT_END = SECOND_BLOCK_INDENT_START + LETTER_SPACING;
+	
+	parameter THIRD_BLOCK_INDENT_START = SECOND_BLOCK_INDENT_END + 70;
+	parameter THIRD_BLOCK_INDENT_END = THIRD_BLOCK_INDENT_START + LETTER_SPACING;
+	
+	parameter FOURTH_BLOCK_INDENT_START = THIRD_BLOCK_INDENT_END + 70;
+	parameter FOURTH_BLOCK_INDENT_END = FOURTH_BLOCK_INDENT_START + LETTER_SPACING;
+	
+	parameter TOP_LETTERS_START = 45;
+	parameter TOP_LETTERS_END = TOP_LETTERS_START + 75;
+
+	parameter BOTTOM_LETTERS_START = 295;
+	parameter BOTTOM_LETTERS_END = BOTTOM_LETTERS_START + 75;
+	
+	
 	always @(*)begin
 		hsync = ~(x >= HS_STA && x < HS_END);  
       vsync = ~(y >= VS_STA && y < VS_END);
 		de = (x <= HA_END && y <= VA_END);	
 	end
 	
+	reg gameOver;
 	//note this is not the proper way to define should be changed
 	//ADXL345 module
 	ADXL345Handler accelerometer(clk1M,MISO,getData,rst,spiclk,chipselect,MOSI,dataXout,dataYout);
@@ -94,46 +118,291 @@ module EECS3201Final(clkin,rst,MISO,hsync,vsync,r,g,b,MOSI,spiclk,chipselect, he
 			targetsize <= 255;
 			counter <= 0;
 			rng_state <= 0;
+			gameOver <= 1'b0;
+
 			
 		end else begin 
 			
 			if(de)begin	
-				//Check if the player object is within the target object.	
-				if(playerx < targetx || playery < targety || (playerx +32) > (targetx + targetsize) || (playery + 32) > (targety + targetsize)  )
-				begin
-						
-					x <= 0;
-					y <= 0;
-					playerx <= 303;
-					playery <= 223;
-					getData <= 0;
-					targetx <= 192;
-					targety <= 112;
-					targetsize <= 255;
-					counter <= 0;
-					rng_state <= 0;
-						
-				end
-			
-				//note non of the draw register should be updated during the de == 1 period 
-				//or artifacts may appear
-				else if(x >= playerx && x <= playerx+32 && y >= playery && y <= playery+32)begin
-					//player is a pink color
-					r <= 4'b1111;
-					g <= 4'b1001;
-					b <= 4'b1100;
-				end else if(x >= targetx && x <= (targetx+targetsize) && y >= targety && y <= (targety+targetsize))begin
-					//target color is blue
-					r <= 4'b0010;
-					g <= 4'b1000;
-					b <= 4'b1111;
-				end else begin
-					//background is a orange color
-					r <= 4'b1111;
-					g <= 4'b1100;
-					b <= 4'b1001;
-				end
 				
+				if(!gameOver) begin
+					//Check if the player object is within the target object.	
+					if(playerx < targetx || playery < targety || (playerx +32) > (targetx + targetsize) || (playery + 32) > (targety + targetsize)  )
+					begin
+						
+						gameOver <= 1'b1;
+						
+					end
+			
+					//note non of the draw register should be updated during the de == 1 period 
+					//or artifacts may appear
+					else if(x >= playerx && x <= playerx+32 && y >= playery && y <= playery+32)begin
+						//player is a pink color
+						r <= 4'b1111;
+						g <= 4'b1001;
+						b <= 4'b1100;
+					end else if(x >= targetx && x <= (targetx+targetsize) && y >= targety && y <= (targety+targetsize))begin
+						//target color is blue
+						r <= 4'b0010;
+						g <= 4'b1000;
+						b <= 4'b1111;
+					end else begin
+						//background is a orange color
+						r <= 4'b1111;
+						g <= 4'b1100;
+						b <= 4'b1001;
+					end
+					
+				end
+				else
+				begin
+					//paint the game over screen
+					if(0 <= y && y < 250)
+					begin
+						//Paint the top letters
+						//Logic for creating the letter 'G'
+						if((FIRST_BLOCK_INDENT_START <= x && x <= FIRST_BLOCK_INDENT_END) && (TOP_LETTERS_START < y && y < TOP_LETTERS_END))
+						begin
+							if((FIRST_BLOCK_INDENT_START <= x && x <= FIRST_BLOCK_INDENT_END) && (TOP_LETTERS_START < y && y < TOP_LETTERS_START + BLOCK_SIZE))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							else if((FIRST_BLOCK_INDENT_START <= x && x < FIRST_BLOCK_INDENT_START + BLOCK_SIZE) && (TOP_LETTERS_START < y && y < TOP_LETTERS_END))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							else if((FIRST_BLOCK_INDENT_START <= x && x <= FIRST_BLOCK_INDENT_END) && (TOP_LETTERS_END - BLOCK_SIZE < y && y < TOP_LETTERS_END))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							else if((FIRST_BLOCK_INDENT_END - BLOCK_SIZE <= x && x <= FIRST_BLOCK_INDENT_END) && (((TOP_LETTERS_START +TOP_LETTERS_END) /2) < y && y < TOP_LETTERS_END))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							else if((((FIRST_BLOCK_INDENT_START + FIRST_BLOCK_INDENT_END) /2) <= x && x <= FIRST_BLOCK_INDENT_END) && (((TOP_LETTERS_START +TOP_LETTERS_END) /2) < y && y < ((TOP_LETTERS_START +TOP_LETTERS_END) /2) + BLOCK_SIZE))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							else
+							begin
+								r <= 4'b0000;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							
+						end
+						//Logic for creating the letter 'A'
+						else if((SECOND_BLOCK_INDENT_START <= x && x <= SECOND_BLOCK_INDENT_END)&& (TOP_LETTERS_START < y && y < TOP_LETTERS_END))
+						begin
+							if((SECOND_BLOCK_INDENT_START <= x && x <= SECOND_BLOCK_INDENT_END) && (TOP_LETTERS_START < y && y < TOP_LETTERS_START + BLOCK_SIZE))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							else if((SECOND_BLOCK_INDENT_START <= x && x < SECOND_BLOCK_INDENT_START + BLOCK_SIZE) && (TOP_LETTERS_START < y && y < TOP_LETTERS_END))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							else if((SECOND_BLOCK_INDENT_END - BLOCK_SIZE<= x && x < SECOND_BLOCK_INDENT_END + BLOCK_SIZE) && (TOP_LETTERS_START < y && y < TOP_LETTERS_END))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							else if((SECOND_BLOCK_INDENT_START <= x && x < SECOND_BLOCK_INDENT_END) && (((TOP_LETTERS_START +TOP_LETTERS_END) /2) < y && y < ((TOP_LETTERS_START +TOP_LETTERS_END) /2) + BLOCK_SIZE))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							else 
+							begin
+								r <= 4'b0000;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+						end
+						//Logic for Creating letter 'M' will go here.
+						else if((THIRD_BLOCK_INDENT_START <= x && x <= THIRD_BLOCK_INDENT_END) && (TOP_LETTERS_START < y && y < TOP_LETTERS_END))
+						begin
+							r <= 4'b1111;
+							g <= 4'b0000;
+							b <= 4'b0000;
+						end
+						//Logic for creating letter 'E'
+						else if((FOURTH_BLOCK_INDENT_START <= x && x <= FOURTH_BLOCK_INDENT_END) && (TOP_LETTERS_START < y && y < TOP_LETTERS_END))
+						begin
+							if((FOURTH_BLOCK_INDENT_START <= x && x <= FOURTH_BLOCK_INDENT_END) && (TOP_LETTERS_START < y && y < TOP_LETTERS_START + BLOCK_SIZE))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							else if((FOURTH_BLOCK_INDENT_START <= x && x < FOURTH_BLOCK_INDENT_START + BLOCK_SIZE) && (TOP_LETTERS_START < y && y < TOP_LETTERS_END))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							else if((FOURTH_BLOCK_INDENT_START <= x && x <= FOURTH_BLOCK_INDENT_END) && (TOP_LETTERS_END - BLOCK_SIZE < y && y < TOP_LETTERS_END))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							else if((FOURTH_BLOCK_INDENT_START <= x && x < FOURTH_BLOCK_INDENT_END) && (((TOP_LETTERS_START +TOP_LETTERS_END) /2) < y && y < ((TOP_LETTERS_START +TOP_LETTERS_END) /2) + BLOCK_SIZE))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							
+							else 
+							begin
+								r <= 4'b0000;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+						end
+	
+						else 
+						begin
+							r <= 4'b0000;
+							g <= 4'b0000;
+							b <= 4'b0000;
+						end
+					end
+					else
+					//Bottom half letters
+					begin
+						//Logic for creating the 'O'
+						if((FIRST_BLOCK_INDENT_START <= x && x <= FIRST_BLOCK_INDENT_END) && (BOTTOM_LETTERS_START < y && y < BOTTOM_LETTERS_END))
+						begin
+							if((FIRST_BLOCK_INDENT_START <= x && x <= FIRST_BLOCK_INDENT_END) && (BOTTOM_LETTERS_START < y && y < BOTTOM_LETTERS_START + BLOCK_SIZE))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							else if((FIRST_BLOCK_INDENT_START <= x && x < FIRST_BLOCK_INDENT_START + BLOCK_SIZE) && (BOTTOM_LETTERS_START < y && y < BOTTOM_LETTERS_END))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							else if((FIRST_BLOCK_INDENT_END - BLOCK_SIZE<= x && x < FIRST_BLOCK_INDENT_END + BLOCK_SIZE) && (BOTTOM_LETTERS_START < y && y < BOTTOM_LETTERS_END))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							else if((FIRST_BLOCK_INDENT_START <= x && x < FIRST_BLOCK_INDENT_END) && (BOTTOM_LETTERS_END - BLOCK_SIZE < y && y < BOTTOM_LETTERS_END))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							else 
+							begin
+								r <= 4'b0000;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+						end
+						//Logic for creating the 'V'
+						else if((SECOND_BLOCK_INDENT_START <= x && x <= SECOND_BLOCK_INDENT_END) && (BOTTOM_LETTERS_START < y && y < BOTTOM_LETTERS_END))
+						begin
+							if((SECOND_BLOCK_INDENT_START <= x && x < SECOND_BLOCK_INDENT_START + BLOCK_SIZE) && (BOTTOM_LETTERS_START < y && y < BOTTOM_LETTERS_END))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							else if((SECOND_BLOCK_INDENT_END - BLOCK_SIZE<= x && x < SECOND_BLOCK_INDENT_END + BLOCK_SIZE) && (BOTTOM_LETTERS_START < y && y < BOTTOM_LETTERS_END))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							else if((SECOND_BLOCK_INDENT_START <= x && x < SECOND_BLOCK_INDENT_END) && (BOTTOM_LETTERS_END - BLOCK_SIZE < y && y < BOTTOM_LETTERS_END))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							else 
+							begin
+								r <= 4'b0000;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+						end
+						//Logic for creating the 'E'
+						else if((THIRD_BLOCK_INDENT_START <= x && x <= THIRD_BLOCK_INDENT_END) && (BOTTOM_LETTERS_START < y && y < BOTTOM_LETTERS_END))
+						begin
+							if((THIRD_BLOCK_INDENT_START <= x && x <= THIRD_BLOCK_INDENT_END) && (BOTTOM_LETTERS_START < y && y < BOTTOM_LETTERS_START + BLOCK_SIZE))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							else if((THIRD_BLOCK_INDENT_START <= x && x < THIRD_BLOCK_INDENT_START + BLOCK_SIZE) && (BOTTOM_LETTERS_START < y && y < BOTTOM_LETTERS_END))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							else if((THIRD_BLOCK_INDENT_START <= x && x <= THIRD_BLOCK_INDENT_END) && (BOTTOM_LETTERS_END - BLOCK_SIZE < y && y < BOTTOM_LETTERS_END))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							else if((THIRD_BLOCK_INDENT_START <= x && x < THIRD_BLOCK_INDENT_END) && (((BOTTOM_LETTERS_START +BOTTOM_LETTERS_END) /2) < y && y < ((BOTTOM_LETTERS_START +BOTTOM_LETTERS_END) /2) + BLOCK_SIZE))
+							begin
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+							
+							else 
+							begin
+								r <= 4'b0000;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end
+						end
+						//Logic for creating the 'R' not done yet.
+						else if((FOURTH_BLOCK_INDENT_START <= x && x <= FOURTH_BLOCK_INDENT_END) && (BOTTOM_LETTERS_START < y && y < BOTTOM_LETTERS_END))
+						begin
+							r <= 4'b1111;
+							g <= 4'b0000;
+							b <= 4'b0000;
+						end
+						else
+						begin
+							r <= 4'b0000;
+							g <= 4'b0000;
+							b <= 4'b0000;
+						end
+						
+					end
+					
+				end
+				//if none of the above are true, paint it as black.
 			end else begin		
 				r <= 4'b0000;
 				g <= 4'b0000;
@@ -141,6 +410,9 @@ module EECS3201Final(clkin,rst,MISO,hsync,vsync,r,g,b,MOSI,spiclk,chipselect, he
 	
 		end
 		
+			
+			
+			
 			//read data from the adxl345 and enable the rng
 			if(y == 480 || y == 481)begin
 				getData <= ~getData;
