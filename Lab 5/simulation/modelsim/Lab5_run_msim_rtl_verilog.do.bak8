b0bb904dbe711dc2d58e767d9025a9f14ad8a134
unknown <jorra04@my.yorku.ca>
2021-11-24 19:38:40 -0500

    Finished lab 5.

---
+++ Lab 5/simulation/modelsim/Lab5_run_msim_rtl_verilog.do.bak8
@@ -0,0 +1,9 @@
+transcript on
+if {[file exists rtl_work]} {
+	vdel -lib rtl_work -all
+}
+vlib rtl_work
+vmap work rtl_work
+
+vlog -vlog01compat -work work +incdir+C:/Users/Jorra/Documents/Coding\ Practice/School/EECS3201/Lab\ 5 {C:/Users/Jorra/Documents/Coding Practice/School/EECS3201/Lab 5/test2.v}
+
--- Lab 5/test2_tb.v
+++ Lab 5/test2_tb.v
@@ -4,7 +4,7 @@ module test2_tb;
 
 	reg clk,a;
 	wire[3:0] r;
-	reg allFalse = 1'b0;
+	reg allFalse = 1'b1;
 	test2 test2_test(
 		.clk(clk),
 		.a(a),
@@ -14,35 +14,30 @@ module test2_tb;
 	
 	
 	always begin
-		assign clk = 1'b1;
-		#20;
+		clk = 1'b1;
+		#10;
 		
-		assign clk = 1'b0;
-		#20;
+		clk = 1'b0;
+		#10;
 	end
 	
+	initial begin
 	
-	
-	always @(posedge clk) begin
-		
-		assign a = 0;
+		a = 0;
 		#100;
-		allFalse = allFalse && (r == 0);
+		allFalse = allFalse && (r==0);
 		
-		assign a = 1;
+		a = 1;
 		#200;
+		allFalse = allFalse && (r==0);
+		
+		a = 0;
 		allFalse = allFalse && (r == 0);
 		
-		assign a = 0;
-		allFalse = allFalse && (r == 0);
-		
-		
-		if(!allFalse)
-				$display("All outputs are zero");
-				
-				
+		if(allFalse)
+			$display("All outputs are zero");
+			
 		$stop;
-		
 		
 	end
 	
